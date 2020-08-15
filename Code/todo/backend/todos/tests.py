0a9264eb11c3b186385b7bc2d94e4f75a742a034
Jesse Hunt <jh.trumpet37@gmail.com>
2020-08-15 18:00:38 +0200

    - removed unnecessary pytest db marks

--- Code/todo/backend/todos/tests.py
+++ Code/todo/backend/todos/tests.py
@@ -1,24 +1,18 @@
 import pytest
 from .models import Todo
 
-pytestmark = pytest.mark.django_db
-
 
 @pytest.mark.django_db
 class TestTodoModel:
-    @pytest.mark.django_db
     @pytest.fixture()
     def todo_object(self):
-        pytestmark = pytest.mark.django_db
         todo_entry = Todo.objects.create(title="test todo", body="todo body")
         return todo_entry.id
 
-    @pytest.mark.django_db
     def test_title_content(self, todo_object):
         todo = Todo.objects.get(id=todo_object)
         assert todo.title == "test todo"
 
-    @pytest.mark.django_db
     def test_body_content(self, todo_object):
         todo = Todo.objects.get(id=todo_object)
         assert todo.body == "todo body"
