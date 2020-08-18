27b800a201c2c7cc7c8e6096cb810ba9f4a9c32e
Jesse Hunt <jh.trumpet37@gmail.com>
2020-08-18 17:20:38 +0200

    - tested with mock data - added axios

--- Code/todo/frontend/src/App.js
+++ Code/todo/frontend/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+    {
+        "id": 1,
+        "title": "Complete API Book Coursework",
+        "body": "Finish all the chapters"
+    },
+    {
+        "id": 2,
+        "title": "Make Mac n Cheese",
+        "body": "Make sure to leave a good hour to get things done"
+    },
+    {
+        "id": 3,
+        "title": "Take a Shower",
+        "body": "You stanky."
+    }
+]
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { list };
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.list.map(item => (
+                    <div key={item.id}>
+                        <h1>{item.title}</h1>
+                        <p>{item.body}</p>
+                    </div>
+                ))}
+            </div>
+        )
+    }
 }
 
 export default App;
